20dfe475922524a6e273b8d0aed1afb669f4a7a7
Michael Gruben <michael.gruben@gmx.net>
2016-05-29 14:11:33 +0200

    korrekte Anzeige von "nächste Leerung"

--- app/src/main/java/de/it_garten/micha/garbagesaver/Overview.java
+++ app/src/main/java/de/it_garten/micha/garbagesaver/Overview.java
@@ -7,50 +7,67 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
-import java.lang.reflect.Array;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.Locale;
 
 public class Overview extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
-        Date[] garbageDate = {
-                new Date(sdf.parse("08.01.2016")),
-                new Date(sdf.parse("21.01.2016")),
-                new Date(sdf.parse("04.02.2016")),
-                new Date(sdf.parse("18.02.2016")),
-                new Date(sdf.parse("03.03.2016")),
-                new Date(sdf.parse("17.01.2016")),
-                new Date(sdf.parse("01.04.2016")),
-                new Date(sdf.parse("14.04.2016")),
-                new Date(sdf.parse("28.04.2016")),
-                new Date(sdf.parse("12.05.2016")),
-                new Date(sdf.parse("27.05.2016")),
-                new Date(sdf.parse("09.06.2016")),
-                new Date(sdf.parse("23.06.2016")),
-                new Date(sdf.parse("07.07.2016")),
-                new Date(sdf.parse("21.07.2016")),
-                new Date(sdf.parse("04.08.2016")),
-                new Date(sdf.parse("19.08.2016")),
-                new Date(sdf.parse("01.09.2016")),
-                new Date(sdf.parse("15.09.2016")),
-                new Date(sdf.parse("29.09.2016")),
-                new Date(sdf.parse("13.10.2016")),
-                new Date(sdf.parse("27.10.2016")),
-                new Date(sdf.parse("10.11.2016")),
-                new Date(sdf.parse("24.11.2016")),
-                new Date(sdf.parse("08.12.2016")),
-                new Date(sdf.parse("22.12.2016"))
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy", Locale.GERMANY);
+        String[] dates = {
+                "08.01.2016",
+                "21.01.2016",
+                "04.02.2016",
+                "18.02.2016",
+                "03.03.2016",
+                "17.01.2016",
+                "01.04.2016",
+                "14.04.2016",
+                "28.04.2016",
+                "12.05.2016",
+                "27.05.2016",
+                "09.06.2016",
+                "23.06.2016",
+                "07.07.2016",
+                "21.07.2016",
+                "04.08.2016",
+                "19.08.2016",
+                "01.09.2016",
+                "15.09.2016",
+                "29.09.2016",
+                "13.10.2016",
+                "27.10.2016",
+                "10.11.2016",
+                "24.11.2016",
+                "08.12.2016",
+                "22.12.2016"
         };
+        Date []garbageDates= new Date[dates.length];
+        for (int i=0; i<dates.length; i++) {
+            try{
+                garbageDates[i]=sdf.parse(dates[i]);
+            } catch (java.text.ParseException e){
+                System.out.println("kann nicht parsen");
+            }
+        }
+
+        long today=new Date().getTime();
+        String next_date="01.01.2016";
+        for (Date garbageDate: garbageDates){
+            if (today<garbageDate.getTime()){
+                next_date= sdf.format(garbageDate);
+                break;
+            }
+        }
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        TextView next_collect = (TextView) findViewById(R.id.next_collect);
+       //System.out.println(next_collect.getText());
+        next_collect.setText(String.valueOf(next_date));
 
         SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
 
